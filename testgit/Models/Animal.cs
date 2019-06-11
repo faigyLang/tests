0a6bb110ae51eadf9d8c6c74d286661562ce52b8
android <android@main.local>
2019-06-11 10:01:50 +0300

    adding class animal!

---
+++ testgit/Models/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testgit.Models
+{
+    public class Animal
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+    }
+}
