b405d61a798525c1b9f394a9fe57960f6b5ad19e
פלד נחמה <207618687>
2019-06-18 09:43:44 +0300

    commited

---
+++ testgit/Controllers/newModelController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testgit.Controllers
+{
+    public class newModelController : Controller
+    {
+        // GET: newModel
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
