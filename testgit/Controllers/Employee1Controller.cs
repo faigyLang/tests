1950f6997688b8563d9fecb4e4b027a22c4ac528
אזראן מיכל <207604844>
2019-06-18 09:47:14 +0300

    added employee

---
+++ testgit/Controllers/Employee1Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testgit.Models;
+
+namespace testgit.Controllers
+{
+    public class Employee1Controller : Controller
+    {
+        // GET: Default1
+        List<employee> people = new List<employee>()
+        {
+            new employee{ID= 1}
+        };
+    }
+}
