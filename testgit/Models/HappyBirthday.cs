9c495ec860d7ae0838606ff8ae14ddbdad30b4ab
כהן מוריה <207832288>
2019-06-18 09:47:01 +0300

    add birthDay model

---
+++ testgit/Models/HappyBirthday.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testgit.Models
+{
+    public class HappyBirthday
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool Gift { get; set; }
+        public string Cake { get; set; }
+    }
+}
